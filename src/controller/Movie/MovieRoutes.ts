408e0be7ad26dd6711ee131f38d7f7167d57bddb
vinhnado0231 <vinh.nd0231@gmail.com>
2023-09-18 19:45:09 +0700

    add new feature

--- src/controller/Movie/MovieRoutes.ts
+++ src/controller/Movie/MovieRoutes.ts
@@ -1,11 +1,22 @@
-import express from 'express';
-import { MovieController } from './MovieController';
+// import express from 'express';
+// import { MovieController } from './MovieController';
 
-const router = express.Router();
+// const router = express.Router();
 
-const movieController = new MovieController();
+// const movieController = new MovieController();
 
-router.get('/movies', movieController.getMovies);
-router.get('/movies/:id', movieController.getMovieById);
+// router.get('/movies', movieController.getMovies);
+// router.get('/movies/:id', movieController.getMovieById);
 
-export default router;
+// export default router;
+
+import BaseRoutes from '..//Base/BaseRouter';
+import MovieController from './MovieController';
+class MovieRoutes extends BaseRoutes {
+	public routes(): void {
+		this.router.get('/movies', MovieController.getMovies);
+		this.router.get('/movies/:id', MovieController.getMovieById);
+	}
+}
+
+export default new MovieRoutes().router;
