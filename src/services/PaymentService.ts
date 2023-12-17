ce68d47047712feaeabdb0eccc5565e65fae52b4
Phuc Personal <phucchp@gmail.com>
2023-12-17 12:20:19 +0700

    api get payments include validate and filter

--- src/services/PaymentService.ts
+++ src/services/PaymentService.ts
@@ -8,6 +8,8 @@ import { IUserRepository } from '../repository/Interfaces/IUserRepository';
 import { Subscription } from '../models/Subscription';
 import { PaymentRepository } from '../repository/PaymentRepository';
 import { Payment } from '../models/Payment';
+import express, { Request } from 'express';
+import { Op } from 'sequelize';
 
 interface PaymentAttributes {
 	type: string;
@@ -27,7 +29,6 @@ export class PaymentService {
 		try {
 			const { transactionId } = paymentData;
 			if (transactionId) {
-				console.log('first');
 				const paymentToUpdate = await this.paymentRepository.findOneByCondition(
 					{
 						transactionId: paymentData.transactionId,
@@ -82,4 +83,68 @@ export class PaymentService {
 			throw new Error(`Failed to find payment: ${error.message}`);
 		}
 	};
+
+	getPayments = async (req: Request) => {
+		try {
+			const status = req.query.status || null;
+			const type = req.query.type || null;
+			const userId = req.query.userId || null;
+			const isPayment = req.query.isPayment || null;
+			const subscriptionInfoId = req.query.subscriptionInfoId || null;
+			const startDate = req.query.startDate || null;
+			const endDate = req.query.endDate || null;
+			const search = req.query.search || null;
+
+			const page = req.query.page ||1;
+			const pageSize = req.query.pageSize ||15;
+			
+			const whereCondition: any = {};
+			if (status) {
+				whereCondition['status'] = { [Op.iLike]: `%${status}%`}
+			}
+
+			if (search) {
+				whereCondition['orderInfo'] = { [Op.iLike]: `%${search}%`}
+			}
+
+			if(type){
+				whereCondition['type'] = type;
+			}
+
+			if(userId){
+				whereCondition['userId'] = userId;
+			}
+
+			if(isPayment){
+				whereCondition['isPayment'] = isPayment;
+			}
+
+			if(subscriptionInfoId){
+				whereCondition['subscriptionInfoId'] = subscriptionInfoId;
+			}
+
+			if (!startDate && !endDate) {
+				whereCondition['createdAt'] = {
+					[Op.between]: [new Date(2020,1,1),new Date()],
+				};
+			}
+
+			if (startDate && !endDate) {
+				whereCondition['createdAt'] = {
+					[Op.between]: [startDate,new Date()],
+				};
+			}
+
+			if (!startDate && endDate) {
+				whereCondition['createdAt'] = {
+					[Op.between]: [new Date(2020,1,1),endDate],
+				};
+			}
+
+			return await this.paymentRepository.getPayments(whereCondition,Number(page),Number(pageSize));
+		} catch (error: any) {
+			throw(error);
+		}
+	};
+
 }
