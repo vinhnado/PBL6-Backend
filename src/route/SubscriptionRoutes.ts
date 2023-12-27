751b51715435cc17a8a9e28ebf61c62a23f96948
vinhnado0231 <vinh.nd0231@gmail.com>
2023-12-27 23:13:11 +0700

    add subscription validate

--- src/route/SubscriptionRoutes.ts
+++ src/route/SubscriptionRoutes.ts
@@ -1,17 +1,19 @@
 import { SubscriptionController } from '../controller/SubscriptionController';
 import BaseRoutes from './Base/BaseRouter';
+import { validateCreateSubscriptionInfo, validateCreateSubscriptionType, validateDeleteSubscriptionInfo, validateDeleteSubscriptionType, validateUpdateSubscription, validateUpdateSubscriptionInfo, validateUpdateSubscriptionType } from '../validators/SubscriptionValidator';
+import { validate } from '../validators/Validator';
 class SubscriptionRoutes extends BaseRoutes {
 	constructor() {
 		super(new SubscriptionController());
 	}
 	public routes(): void {
-		this.router.put('/update-subscription', this.controller.updateSubscription);
+		this.router.put('/update-subscription',validateUpdateSubscription,validate, this.controller.updateSubscription);
 		this.router.post(
-			'/create-subscription-type',
+			'/create-subscription-type',validateCreateSubscriptionType,validate,
 			this.controller.createSubscriptionType
 		);
 		this.router.put(
-			'/update-subscription-type',
+			'/update-subscription-type',validateUpdateSubscriptionType,validate,
 			this.controller.updateSubscriptionType
 		);
 		this.router.get(
@@ -19,7 +21,7 @@ class SubscriptionRoutes extends BaseRoutes {
 			this.controller.getAllSubscriptionType
 		);
 		this.router.delete(
-			'/delete-subscription-type',
+			'/delete-subscription-type',validateDeleteSubscriptionType,validate,
 			this.controller.deleteSubscriptionType
 		);
 		this.router.get(
@@ -27,15 +29,15 @@ class SubscriptionRoutes extends BaseRoutes {
 			this.controller.getAllSubscriptionInfo
 		);
 		this.router.post(
-			'/create-subscription-info',
+			'/create-subscription-info',validateCreateSubscriptionInfo,validate,
 			this.controller.createSubscriptionInfo
 		);
 		this.router.put(
-			'/update-subscription-info',
+			'/update-subscription-info',validateUpdateSubscriptionInfo,validate,
 			this.controller.updateSubscriptionInfo
 		);
 		this.router.delete(
-			'/delete-subscription-info',
+			'/delete-subscription-info',validateDeleteSubscriptionInfo,validate,
 			this.controller.deleteSubscriptionInfo
 		);
 	}
