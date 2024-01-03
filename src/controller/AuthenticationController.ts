cb70330974fd88665c21dfed2b7a3019922e7ee1
vinhnado0231 <vinh.nd0231@gmail.com>
2024-01-03 22:20:41 +0700

    add send mail after register

--- src/controller/AuthenticationController.ts
+++ src/controller/AuthenticationController.ts
@@ -80,10 +80,12 @@ export class AuthenticationController {
 				username,
 				password
 			);
-
+			let data = await this.authenticationService.activeUser(email,null);
+			
 			return res.status(200).json({
 				status: 'Ok!',
 				message: 'Successfully registerd users!',
+				message_active: data
 			});
 		} catch (error: any) {
 			handleErrorController(error, res);
@@ -103,9 +105,12 @@ export class AuthenticationController {
 				true
 			);
 
+			let data = await this.authenticationService.activeUser(email,null);
+			
 			return res.status(200).json({
-				status: '200',
-				message: 'Successfully registered admin!',
+				status: 'Ok!',
+				message: 'Successfully registerd admin!',
+				message_active: data
 			});
 		} catch (error: any) {
 			handleErrorController(error, res);
